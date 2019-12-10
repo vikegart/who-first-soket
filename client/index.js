52015db1c3829ef5ad6f00af8a3fb044c9cec3fe
Vikentii Ekgart <Vikentii_Ekgart@epam.com>
2019-12-10 15:26:25 +0400

    init libs, started files for server and client

---
+++ client/index.js
@@ -0,0 +1,4 @@
+setTimeout(()=>{
+    const body = document.getElementsByTagName('body')[0];
+    body.innerHTML = "hElO wrdl 🤪";
+}, 300);
---
+++ server/index.js
@@ -0,0 +1,45 @@
+const app = require("express")();
+const http = require("http").Server(app);
+const path = require('path');
+const cors = require('cors');
+
+const io = require("socket.io")(http);
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+
+let Usercounter = 0;
+
+const voicesArr = [];
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, '../client/index.html'));
+});
+
+app.get('/index.js', function(req, res) {
+  res.sendFile((path.join(__dirname, '../client/index.js')));
+});
+
+io.on("connection", function(socket) {
+  Usercounter = Usercounter + 1;
+  io.emit("user", Usercounter);
+  console.log("user is connected");
+  socket.on("disconnect", function() {
+    Usercounter = Usercounter - 1;
+    io.emit("user", Usercounter);
+    console.log("user disconnected");
+  });
+
+  socket.on("audioMessage", function(msg) {
+    io.emit("audioMessage", msg);
+    voicesArr.push({timeStamp: Date(), audioBlob: msg});
+  });
+
+  socket.on("recordStarted", () => {
+    io.emit("playStarSound");
+  })
+});
+
+http.listen(port, function() {
+  console.log("listening to port:" + port);
+});
