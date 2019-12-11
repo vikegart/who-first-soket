f43b3fda2b2494a296e53b1afc626b9f771f1873
Vikentii Ekgart <Vikentii_Ekgart@epam.com>
2019-12-11 11:50:14 +0400

    working client side

--- server/index.js
+++ server/index.js
@@ -8,16 +8,47 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 
+const routerFilesMap = {
+  host: '/pages/hostPage/',
+  player: '/pages/playerPage/',
+  shared: '/shared/',
+}
+
 let Usercounter = 0;
 
 const voicesArr = [];
 
+app.get('/shared/shared.css', function(req, res) {
+  res.sendFile((path.join(__dirname, `../client/${routerFilesMap.shared}shared.css`)));
+});
+
+app.get('/shared/normalize.css', function(req, res) {
+  res.sendFile((path.join(__dirname, `../client/${routerFilesMap.shared}normalize.css`)));
+});
+
+
+
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, '../client/index.html'));
+  res.sendFile(path.join(__dirname, `../client/${routerFilesMap.player}index.html`));
 });
 
 app.get('/index.js', function(req, res) {
-  res.sendFile((path.join(__dirname, '../client/index.js')));
+  res.sendFile((path.join(__dirname, `../client/${routerFilesMap.player}index.js`)));
+});
+
+app.get('/index.css', function(req, res) {
+  res.sendFile((path.join(__dirname, `../client/${routerFilesMap.player}index.css`)));
+});
+
+
+
+
+app.get("/chto/", function(req, res) {
+  res.sendFile(path.join(__dirname, `../client/${routerFilesMap.host}index.html`));
+});
+
+app.get('/chto/index.js', function(req, res) {
+  res.sendFile((path.join(__dirname, `../client/${routerFilesMap.host}index.js`)));
 });
 
 io.on("connection", function(socket) {
