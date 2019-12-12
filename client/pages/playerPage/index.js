b6ac4869de97abef4d34624f0dfab6dca17e4e44
Aleksandr Ivanov <aleksandr_ivanov4@epam.com>
2019-12-12 09:16:50 +0400

    FE: Add vibration on player click and restyled answer layout

--- client/pages/playerPage/index.js
+++ client/pages/playerPage/index.js
@@ -11,25 +11,38 @@ const answerButton = document.getElementsByClassName("answer-button")[0];
 let name = "";
 let ready = false;
 
-const onLoginClick = e => {    
+const onLoginClick = e => {
     const inputValue = nameInput.value;
     if (!inputValue || !inputValue.trim()) return;
     name = inputValue;
     nameGroup.classList.add("hidden");
     answerGroup.classList.remove("hidden");
-}
+};
 
 const onLogoutClick = e => {
     nameGroup.classList.remove("hidden");
     answerGroup.classList.add("hidden");
-}
+};
 
-const onAnswerClick = e => ready && socket.emit("answer", name);
+const onAnswerClick = e => {
+    if (ready) {
+        window.navigator.vibrate && window.navigator.vibrate(1000);
+        ready && socket.emit("answer", name);
+    } else {
+        window.navigator.vibrate && window.navigator.vibrate([300, 100, 300]);
+    }
+};
 
 loginButton.addEventListener("click", onLoginClick);
 logoutButton.addEventListener("click", onLogoutClick);
 answerButton.addEventListener("click", onAnswerClick);
 
 socket.on('ready', isReady => (ready = isReady));
+socket.on('tried', () => { 
+    if (window.navigator.vibrate) {
+        window.navigator.vibrate(0); 
+        window.navigator.vibrate([300, 100, 300]); 
+    }
+});
 
 
